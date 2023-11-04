d8b1b2c4877964fb29a7a496c92ae396a76b6b70
Essawey <M.Abdelmaged@nu.edu.eg>
2023-11-04 20:18:34 +0200

    change the path of the cleaned dataset

--- final.sh
+++ final.sh
@@ -1,6 +1,6 @@
 docker cp essawey-container:/home/doc-bd-a1/eda-in-1.txt service-result/
 docker cp essawey-container:/home/doc-bd-a1/k.txt service-result/
 docker cp essawey-container:/home/doc-bd-a1/vis.png service-result/
-docker cp essawey-container:/home/doc-bd-a1/titanic.csv service-result/
+docker cp essawey-container:/home/doc-bd-a1/res_dpre.csv service-result/
 
 docker essawey-container stop
